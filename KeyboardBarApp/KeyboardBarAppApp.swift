de017c877f8d9c09e46effe3d40d1667a8e0067c
Bill Calkins <bill@calkinsconsulting.com>
2024-09-28 14:19:07 -0500

    Initial Commit

---
+++ KeyboardBarApp/KeyboardBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardBarAppApp.swift
+//  KeyboardBarApp
+//
+//  Created by Bill Calkins on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyboardBarAppTests/KeyboardBarAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardBarAppTests.swift
+//  KeyboardBarAppTests
+//
+//  Created by Bill Calkins on 9/28/24.
+//
+
+import Testing
+@testable import KeyboardBarApp
+
+struct KeyboardBarAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

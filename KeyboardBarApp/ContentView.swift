ea25b2e167ee39a2b04ed315b3ac7d5b0f5d0e23
Bill Calkins <bill@calkinsconsulting.com>
2024-10-03 13:13:21 -0500

    Tried more stuff

--- KeyboardBarApp/ContentView.swift
+++ KeyboardBarApp/ContentView.swift
@@ -8,113 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var numberInput: String = ""
-    @State private var stringInput: String = ""
-    @State private var isKeyboardVisible: Bool = false
-    @State private var keyboardHeight: CGFloat = 0
-    
-    private var currencyFormatter: NumberFormatter {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.currencyCode = "USD"
-        formatter.maximumFractionDigits = 2
-        formatter.minimumFractionDigits = 2
-        return formatter
-    }
-    
-    var body: some View {
-        VStack {
-            TextField("Enter a number", text: Binding(
-              get: {
-                return numberInput
-            },
-              set: { newValue in
-                let justDigits = newValue.justDigits
-                if justDigits.count <= 10 {
-                  numberInput = newValue.asCurrencyString
-                } else {
-                    numberInput.removeLast()
-                }
-            }))
-            .keyboardType(.numberPad)
-            .padding()
-            .textFieldStyle(.roundedBorder)
-            TextField("Enter a string", text: $stringInput)
-                .keyboardType(.default)
-                .padding()
-                .textFieldStyle(.roundedBorder)
-            Text("Keyboard height: \(keyboardHeight)")
-                .font(.caption)
-            Text("Keyboard is \(isKeyboardVisible ? "visible" : "hidden")")
-            Text("Number: \(numberInput)")
-                .font(.caption)
-            Spacer()
-            if isKeyboardVisible {
-                SearchButtonView {
-                    print("Clicked Me!")
-                    UIApplication.shared.hideKeyboard()
-                    //successfulCompletion()
-                }
-                //.transition(.move(edge: .bottom)) // Transition effect
-            }
-        }
+  
+  @State private var numberInput: String = ""
+  @State private var formattedNumberInput: String = "$0.00"
+  @State private var stringInput: String = ""
+  @State private var isKeyboardVisible: Bool = false
+  @State private var keyboardHeight: CGFloat = 0
+  
+  private var currencyFormatter: NumberFormatter {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    formatter.currencyCode = "USD"
+    formatter.maximumFractionDigits = 2
+    formatter.minimumFractionDigits = 2
+    return formatter
+  }
+  
+  var body: some View {
+    VStack {
+      TextField("Enter a number", text: Binding(
+        get: { formattedNumberInput },
+        set: { newValue in
+          let cleanInput = newValue.justDigits
+          if cleanInput.count <= 10 {
+            numberInput = cleanInput
+            formattedNumberInput = cleanInput.asCurrencyString
+          } else {
+            numberInput = ""
+            formattedNumberInput = "0".asCurrencyString
+          }
+        }))
+      .keyboardType(.decimalPad)
+      .padding()
+      .textFieldStyle(.roundedBorder)
+      
+      TextField("Enter a string", text: $stringInput)
+        .keyboardType(.default)
         .padding()
-        .background(Color.gray.opacity(0.5))
-        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidShowNotification)) { notification in
-            if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
-                withAnimation {
-                    self.isKeyboardVisible = true
-                    self.keyboardHeight = keyboardFrame.height
-                }
-            }
+        .textFieldStyle(.roundedBorder)
+      
+      Text("Keyboard height: \(keyboardHeight)")
+        .font(.caption)
+      Text("Keyboard is \(isKeyboardVisible ? "visible" : "hidden")")
+      Text("numberInput: \(numberInput)")
+        .font(.caption)
+      Text("formattedNumberInput: \(formattedNumberInput)")
+        .font(.caption)
+      Spacer()
+      if isKeyboardVisible {
+        SearchButtonView {
+          print("Clicked Me!")
+          UIApplication.shared.hideKeyboard()
+          //successfulCompletion()
         }
-        .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidHideNotification)) { _ in
-            withAnimation {
-                self.isKeyboardVisible = false
-                self.keyboardHeight = 0
-            }
-        }
+        //.transition(.move(edge: .bottom)) // Transition effect
+      }
     }
+    .padding()
+    .background(Color.gray.opacity(0.5))
+    .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidShowNotification)) { notification in
+      if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+        withAnimation {
+          self.isKeyboardVisible = true
+          self.keyboardHeight = keyboardFrame.height
+        }
+      }
+    }
+    .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardDidHideNotification)) { _ in
+      withAnimation {
+        self.isKeyboardVisible = false
+        self.keyboardHeight = 0
+      }
+    }
+  }
 }
 
 struct KeyboardBarView: View {
-    var successfulCompletion: () -> Void
-    var body: some View {
-        HStack {
-            Spacer()
-            VStack (alignment: .trailing) {
-                SearchButtonView {
-                    print("Clicked Me!")
-                    UIApplication.shared.hideKeyboard()
-                    successfulCompletion()
-                }
-            }
+  var successfulCompletion: () -> Void
+  var body: some View {
+    HStack {
+      Spacer()
+      VStack (alignment: .trailing) {
+        SearchButtonView {
+          print("Clicked Me!")
+          UIApplication.shared.hideKeyboard()
+          successfulCompletion()
         }
-        .frame(height: 38)
-        .padding()
-        .cornerRadius(8) // Optional: To match the corner radius of the overlay
-        .background(Color.gray)
-        .overlay(
-            RoundedRectangle(cornerRadius: 8)
-                .stroke(Color.blue, lineWidth: 1)
-        )
+      }
     }
+    .frame(height: 38)
+    .padding()
+    .cornerRadius(8) // Optional: To match the corner radius of the overlay
+    .background(Color.gray)
+    .overlay(
+      RoundedRectangle(cornerRadius: 8)
+        .stroke(Color.blue, lineWidth: 1)
+    )
+  }
 }
 
 extension UIApplication {
-    func hideKeyboard() {
-        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
+  func hideKeyboard() {
+    sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+  }
 }
 extension String {
-    var justDigits: String {
-        components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
-    }
+  var justDigits: String {
+    components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+  }
   
   var asCurrencyString: String {
     let numberFormatter = NumberFormatter()
-
+    
     numberFormatter.numberStyle = .currency
     numberFormatter.minimumFractionDigits = 2
     numberFormatter.maximumFractionDigits = 2
@@ -131,11 +136,11 @@ extension String {
 
 
 #Preview {
-    ContentView()
+  ContentView()
 }
 
 #Preview {
-    KeyboardBarView() {
-        print("Successfully Completed")
-    }
+  KeyboardBarView() {
+    print("Successfully Completed")
+  }
 }
---
+++ KeyboardBarApp/ContentViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentViewModel.swift
+//  KeyboardBarApp
+//
+//  Created by Bill Calkins on 10/3/24.
+//
+
+import Foundation
+
+final class ContentViewModel: ObservableObject {
+  @Published var combineNumberInput: String = ""
+  
+  
+}
